3cfff5463589a2f2f4043b7a593ed43d194ee7c7
Monika <412.kri@gmail.com>
2023-07-13 20:35:50 +0300

    database

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,5 +1,6 @@
 import http, { IncomingMessage, ServerResponse } from 'node:http';
 import { StringDecoder } from 'node:string_decoder';
+import { Connection } from 'mysql2/promise';
 import { file } from './file.js';
 
 // PAGES
@@ -14,11 +15,26 @@ import { PageAccount } from '../pages/PageAccount.js';
 import { registerAPI } from '../api/register.js';
 import { loginAPI } from '../api/login.js';
 import { cookieParser, isUserLoggedIn } from './utils.js';
+import { servicesAPI } from '../api/services.js';
+
+let dbConnection = {} as Connection;
 
 export type APIresponse = {
     statusCode: number,
     headers: Record<string, any>,
     body: string | undefined,
+}
+
+export type DataForHandlers = {
+    dbConnection: Connection;
+    httpMethod: string;
+    trimmedPath: string;
+    parsedUrl: URL;
+    payload: any;
+    user: {
+        email: string;
+        isLoggedIn: boolean;
+    };
 }
 
 const serverLogic = async (req: IncomingMessage, res: ServerResponse) => {
@@ -74,6 +90,23 @@ const serverLogic = async (req: IncomingMessage, res: ServerResponse) => {
     req.on('end', async () => {
         buffer += stringDecoder.end();
 
+        let jsonData = {};
+        try {
+                jsonData = JSON.parse(buffer);
+            } catch (error) { }
+
+        const dataForHandlers: DataForHandlers = {
+            dbConnection,
+            httpMethod,
+            trimmedPath,
+            parsedUrl,
+            payload: jsonData,
+            user: {
+                email: '',
+                isLoggedIn: false,
+            }
+        }
+
         if (isTextFile) {
             const [err, msg] = await file.readPublic(trimmedPath);
 
@@ -107,10 +140,6 @@ const serverLogic = async (req: IncomingMessage, res: ServerResponse) => {
                 'Content-Type': MIMES.json,
             };
             let apiRes = {} as APIresponse;
-            let jsonData = {};
-            try {
-                jsonData = JSON.parse(buffer);
-            } catch (error) { }
 
             const [_, endpoint, ...restUrlParts] = trimmedPath.split('/') as [string, string, string[]];
             const apiFunction = apiEndpoints[endpoint];
@@ -170,11 +199,13 @@ export const protectedPages: Record<string, any> = {
 export const apiEndpoints: Record<string, any> = {
     'register': registerAPI,
     'login': loginAPI,
+    'services': servicesAPI,
 };
 
 const httpServer = http.createServer(serverLogic);
 
-export const init = () => {
+export const init = (dbConnectionObj: Connection) => {
+    dbConnection = dbConnectionObj;
     httpServer.listen(4415, () => {
         console.log(`Server running at http://localhost:4415`);
     })
